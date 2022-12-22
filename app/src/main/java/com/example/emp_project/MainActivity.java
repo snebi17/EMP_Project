6c1c9265b2e46b7ebf037c8c830bfcdac389f4b2
Timotej <tim.suste@gmail.com>
2022-12-22 18:37:31 +0100

    ferkvenca done :)

--- app/src/main/java/com/example/emp_project/MainActivity.java
+++ app/src/main/java/com/example/emp_project/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.emp_project;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,14 +16,19 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.emp_project.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
+
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.*;
+import be.tarsos.dsp.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    private TextView noteText;
+    private TextView pitchText;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -39,8 +44,70 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        TextView freq = findViewById(R.id.freq_Number2);
-        freq.setText("420");
+        pitchText = findViewById(R.id.freq);
+        noteText = findViewById(R.id.freq_Number2);
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult res, AudioEvent e){
+                final float pitchInHz = res.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPitch(pitchInHz);
+                    }
+                });
+            }
+        };
+
+        AudioProcessor pitchProcessor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(pitchProcessor);
+
+        Thread audioThread = new Thread(dispatcher, "Audio Thread");
+        audioThread.start();
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void processPitch(float pitchInHz) {
+        pitchText.setText("" + pitchInHz);
+
+        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+            //A
+            noteText.setText("A");
+        }
+        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+            //B
+            noteText.setText("B");
+        }
+        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+            //C
+            noteText.setText("C");
+        }
+        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+            //D
+            noteText.setText("D");
+        }
+        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+            //E
+            noteText.setText("E");
+        }
+        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+            //F
+            noteText.setText("F");
+        }
+        else if(pitchInHz >= 185 && pitchInHz < 196) {
+            //G
+            noteText.setText("G");
+        }
+        else if(pitchInHz > 196) {
+            noteText.setText("to high");
+        }
+        else {
+            noteText.setText("to low");
+        }
     }
 
     @Override
